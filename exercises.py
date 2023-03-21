171dfb15d7e4d8eeb4e64a291ef0e7961a7b027d
Kirill Svidunovich <Altair21817@gmail.com>
2023-03-21 14:48:59 +0300

    Add couple exc

--- exercises.py
+++ exercises.py
@@ -671,28 +671,134 @@ from decorators import time_measure_decorator
 # if __name__ == '__main__':
 #     main()
 
-""""""
+"""Задача.
+Количество учеников N пришло на контрольную. Все парты стоят в одну колонну.
+Учитель подготовил K вариантов заданий. За каждой партой (кроме, возможно,
+последней) будут находиться ровно 2 ученика. Варианты будут раздаваться строго
+по порядку: левый ученик первой парты получит 1й вариант, правый ученик первой
+парты второй, левый ученик второй парты получит 3й вариант (N > 2) и т.д.
+Отличник вошел первый в класс и занял случайное место. Двоечник хочет получить
+такой же вариант, как и отличник, и обязательно сидеть в ряду позади, а не
+перед отличником, при этом занять место как можно ближе. В ответе через запятую
+укажите номер ряда и номер позиции за партой (1 - слева, 2 - справа), куда
+необходимо сесть двоечнику. Если такого места нет, укажите "-1".
+В первой строке ввода находится количество учеников N. Во второй строке -
+количество подготовленных вариантов работ K. В третьей строке - номер ряда,
+куда сел отличник, в четвертой - положение за партой (1 - слева, 2 - справа).
+"""
+
+# """Примеры с ответами для тестов."""
+# INPUT_1 = [  # 2 2
+#     25,
+#     2,
+#     1,
+#     2]
+# INPUT_2 = [  # -1
+#     25,
+#     13,
+#     7,
+#     1]
+
+# INPUT = INPUT_1
 
 
+# def find_nearest_seat(
+#         students_count: int,
+#         exc_count: int,
+#         lookup_row: int,
+#         lookup_seat: int) -> str or int:
+#     lookup_seat_num: int = lookup_seat + 2 * (lookup_row - 1)
+#     lookup_exc: int = lookup_seat_num % exc_count
+#     if lookup_exc == 0:
+#         lookup_exc = exc_count
+#     target_seat_nym: int = lookup_seat_num + exc_count
+#     if target_seat_nym > students_count:
+#         return -1
+#     target_row_seat: tuple(int, int) = divmod(target_seat_nym, 2)
+#     if target_row_seat[1] == 0:
+#         result: str = f'{target_row_seat[0]} 2'
+#     else:
+#         result: str = f'{target_row_seat[0] + 1} 1'
+#     return result
 
 
+# def main():
+#     students_count, exc_count, lookup_row, lookup_seat = INPUT
+#     print(find_nearest_seat(
+#         students_count=students_count,
+#         exc_count=exc_count,
+#         lookup_row=lookup_row,
+#         lookup_seat=lookup_seat))
 
 
+# if __name__ == '__main__':
+#     main()
+
+"""Хаотичность погоды за n дней понимается количество дней, в которые
+температура строго больше чем в день до (если существует) и в день после (если
+существует). Необходимо вывести единственным числом хаотичность погоды за
+данный период. В первой строке ввода указывается количество дней, во второй -
+температуры. Единственный день в периоде также является хаотичным."""
+
+# """Примеры с ответами для тестов."""
+# INPUT_1: list[str] = [  # 3
+#     '7',
+#     '-1 -10 -8 0 2 0 5'
+# ]
+# INPUT_2: list[str] = [  # 2
+#     '5',
+#     '1 2 5 4 8'
+# ]
+# INPUT_3: list[str] = [  # 1
+#     '1',
+#     '0'
+# ]
+# INPUT_4: list[str] = [  # 0
+#     '5',
+#     '1 1 1 1 1'
+# ]
+# INPUT_5: list[str] = [  # 0
+#     '5',
+#     '1 1 0 1 1'
+# ]
+
+# INPUT: list[str] = INPUT_1
 
 
+# def read_input() -> tuple[int, list[int]]:
+#     """Read input data"""
+#     cnt_days: int = int(INPUT[0])
+#     temps: list[int] = list(map(int, INPUT[1].split()))
+#     return cnt_days, temps
 
 
+# def count_chaos(cnt_days: int, temps: list[int]):
+#     """Count temperature chaos in given period."""
+#     if cnt_days == 1:
+#         return 1
+#     elif cnt_days == 2:
+#         if temps[0] != temps[1]:
+#             return 1
+#         return 0
+#     result: int = 0
+#     if temps[0] > temps[1]:
+#         result += 1
+#     for day_index in range(1, cnt_days - 1):
+#         if (temps[day_index] > temps[day_index - 1]
+#                 and temps[day_index] > temps[day_index + 1]):
+#             result += 1
+#     if temps[cnt_days - 1] > temps[cnt_days - 2]:
+#         result += 1
+#     return result
 
 
+# def main():
+#     cnt_days, temps = read_input()
+#     print(count_chaos(cnt_days, temps))
 
 
-
-
-
-
-
-
-
+# if __name__ == '__main__':
+#     main()
 
 # """Задача.
 # Красотой строки назовем максимальное число идущих подряд одинаковых букв.
