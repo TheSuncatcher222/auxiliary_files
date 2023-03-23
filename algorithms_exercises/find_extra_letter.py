91ccdb05b86fa1138cccb4f7b0d6142ee48f4dcf
Kirill Svidunovich <Altair21817@gmail.com>
2023-03-23 21:01:03 +0300

    Rename letter to char

--- algorithms_exercises/find_extra_letter.py
+++ algorithms_exercises/find_extra_letter.py
@@ -18,6 +18,7 @@ INPUT: list[str] = INPUT_1
 
 
 def count_chars(text):
+    """Count chars by counting."""
     char_dict: dict = {}
     for char in text:
         if char not in char_dict:
@@ -26,7 +27,8 @@ def count_chars(text):
     return char_dict
 
 
-def find_letter(text_1, text_2):
+def find_char(text_1, text_2):
+    """Find extra char."""
     chars_1: dict[str, int] = count_chars(text_1)
     chars_2: dict[str, int] = count_chars(text_2)
     for char in chars_2:
@@ -36,7 +38,7 @@ def find_letter(text_1, text_2):
 
 def main():
     text_1, text_2 = INPUT
-    print(find_letter(text_1=text_1, text_2=text_2))
+    print(find_char(text_1=text_1, text_2=text_2))
 
 
 if __name__ == '__main__':
