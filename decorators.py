ac7e7e95d7507b29d4f1ce347159beb5cf894181
Kirill Svidunovich <Altair21817@gmail.com>
2023-08-11 00:55:43 +0300

    Add decorator_function example

--- decorators.py
+++ decorators.py
@@ -1,7 +1,8 @@
 import time
 
-"""Декоратор измерения времени выполнения функции."""
+
 def time_measure_decorator(func):
+    """Декоратор измерения времени выполнения функции."""
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
@@ -9,3 +10,21 @@ def time_measure_decorator(func):
         print(f'Время выполнения {func.__name__}: {end - start} сек.')
         return result
     return wrapper
+
+
+if __name__ == '__main__':
+    """Пример для пояснения алгоритма работы декораторов."""
+
+    def decorator_function(func):
+        def wrapper(*args, **kwargs):
+            print('Выполняем обёрнутую функцию...')
+            func(*args, **kwargs)
+            print('Выходим из обёртки')
+        return wrapper
+
+    @decorator_function
+    def hello_world(a, b):
+        print(a)
+        print(b)
+
+    hello_world(a=100, b=2)
